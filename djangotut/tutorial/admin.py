6784c399415b7772ed7b1867d5f62cdfaa10f947
zain7404 <zainkhalid6810089@gmail.com>
2025-02-05 19:37:21 +0500

    learn about model relations

--- djangotut/tutorial/admin.py
+++ djangotut/tutorial/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from .models import courseType
+from .models import courseType, courseReview, courseStore ,courseInstructor
+
+class courseReviewInline(admin.TabularInline):
+    model = courseReview
+    extra = 2
+
+class courseTypeAdmin(admin.ModelAdmin):
+    list_display = ('name','type','date_added')
+    inlines = [courseReviewInline]
+
+class storeAdmin(admin.ModelAdmin):
+    list_display = ('name','location')
+    filter_horizontal = ('course_types',)
+
+class instructorAdmin(admin.ModelAdmin):
+    list_display = ('instructor_name','course','valid_until')
 
 # Register your models here.
-admin.site.register(courseType)
+admin.site.register(courseType,courseTypeAdmin)
+admin.site.register(courseStore,storeAdmin)
+admin.site.register(courseInstructor,instructorAdmin)
--- djangotut/tutorial/models.py
+++ djangotut/tutorial/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -17,5 +18,41 @@ class courseType(models.Model):
 
     def __str__(self):
         return self.name
+    
+# one to many
+
+class courseReview(models.Model):
+    course = models.ForeignKey(courseType,on_delete=models.CASCADE,related_name='reviews')
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    rating = models.IntegerField()  
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} review for {self.course.name}'
+    
+# many to many
+
+class courseStore(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    course_types = models.ManyToManyField(courseType , related_name="stores")
+
+    def __str__(self):
+        return self.name
+    
+# one to one
+
+class courseInstructor(models.Model):
+    course = models.OneToOneField(courseType,on_delete=models.CASCADE, related_name='instructor')
+    instructor_name = models.CharField(max_length=100)
+    instructor_bio = models.TextField()
+    valid_until = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.instructor_name} - {self.course.name}'
+
+
+
 
 
